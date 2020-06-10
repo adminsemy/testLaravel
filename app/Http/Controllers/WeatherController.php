35a9b2852bc3f8c8b4d0b82334d986ec4455ad3f
Antonov.DA <den_ru1982@mail.ru>
2020-06-10 14:12:42 +0300

    Сделаны методы для поиска информации о погоде

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Services\ApiYandexWeather;
 use Illuminate\Http\Request;
 
 class WeatherController extends Controller
 {
     public function show()
     {
-        return view('weather.index');
+        $apiYandex = new ApiYandexWeather();
+        $responseBody = $apiYandex->showWeatherCity('53.2488063', '34.2492743');
+        return view('weather.index', [
+            'responseBody' => $responseBody
+        ]);
     }
 }
